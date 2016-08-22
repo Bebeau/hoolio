613965c3ae16d992c3534be9d77647d6b744bd78
Bebeau <kyle@theinitgroup.com>
2016-08-22 16:43:58 -0700

    add user css

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ function remove_menus(){
     // remove_menu_page( 'edit.php' );
     // remove_menu_page( 'edit.php?post_type=page' );
     remove_menu_page( 'edit-comments.php' );
-    remove_menu_page( 'tools.php' );
+    // remove_menu_page( 'tools.php' );
     // remove_menu_page( 'themes.php' );
     remove_menu_page( 'plugins.php' );
     // remove_menu_page( 'users.php' );
