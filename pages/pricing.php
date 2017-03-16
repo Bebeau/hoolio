a155155dae094f8f51bdf74872bf3f6b424e24b3
Bebeau <kyle@theinitgroup.com>
2017-03-16 09:24:29 -0700

    remove pagebanner, align pricing tiers, and remove testimonials from footer.

--- pages/pricing.php
+++ pages/pricing.php
@@ -22,51 +22,39 @@ $contentTitle = get_post_meta($post->ID,'contentTitle', true);
 		}
 	?>
 	<article class="green">
-		<div class="outer">
-			<div class="inner">
-				<h2>Professional Plus</h2>
-				<p class="price">$499+<span>a month billed annually</span></p>
-				<p>All professional features plus:</p>
-				<ul>
-					<li>10 hours of advisory services per month.</li>
-					<li>Assistance designing and impleimenting surveys.</li>
-					<li>Assistance improving survey based on previous results.</li>
-				</ul>
-				<a href="<?php echo site_url('/contact'); ?>" class="btn">Get In Touch</a>
-			</div>
-		</div>
+		<h2>Professional Plus</h2>
+		<p class="price">$499+<span>a month billed annually</span></p>
+		<p>All professional features plus:</p>
+		<ul>
+			<li>10 hours of advisory services per month.</li>
+			<li>Assistance designing and impleimenting surveys.</li>
+			<li>Assistance improving survey based on previous results.</li>
+		</ul>
+		<a href="<?php echo site_url('/contact'); ?>" class="btn">Get In Touch</a>
 	</article>
 	<article class="blue">
-		<div class="outer">
-			<div class="inner">
-				<h2>Professional</h2>
-				<p class="price">$99<span>a month billed annually</span></p>
-				<a href="https://editor.wyzerr.com/signup" class="btn">Get Started</a>
-				<ul>
-					<li>Easy to use survey editor</li>
-					<li>Fun and Fast Smart forms</li>
-					<li>Innovative Dashboard for quickfeedback analysis</li>
-					<li>Unlimited Surveys</li>
-					<li>Unlimited Responses</li>
-				</ul>
-			</div>
-		</div>
+		<h2>Professional</h2>
+		<p class="price">$99<span>a month billed annually</span></p>
+		<a href="https://editor.wyzerr.com/signup" class="btn">Get Started</a>
+		<ul>
+			<li>Easy to use survey editor</li>
+			<li>Fun and Fast Smart forms</li>
+			<li>Innovative Dashboard for quickfeedback analysis</li>
+			<li>Unlimited Surveys</li>
+			<li>Unlimited Responses</li>
+		</ul>
 	</article>
 	<article class="grey">
-		<div class="outer">
-			<div class="inner">
-				<h2>Enterprise</h2>
-				<p class="price">Custom<span>priced based on needs</span></p>
-				<p>Full survey solution including:</p>
-				<ul>
-					<li>Discovery</li>
-					<li>Design</li>
-					<li>Delivery</li>
-					<li>Iterative learning and data analysis with data scientist</li>
-				</ul>
-				<a href="<?php echo site_url('/contact'); ?>" class="btn">Get In Touch</a>
-			</div>
-		</div>
+		<h2>Enterprise</h2>
+		<p class="price">Custom<span>priced based on needs</span></p>
+		<p>Full survey solution including:</p>
+		<ul>
+			<li>Discovery</li>
+			<li>Design</li>
+			<li>Delivery</li>
+			<li>Iterative learning and data analysis with data scientist</li>
+		</ul>
+		<a href="<?php echo site_url('/contact'); ?>" class="btn">Get In Touch</a>
 	</article>
 </section>
 
